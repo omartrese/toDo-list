6607890e27ad1cabe93c445404e7ce5cfdf2f723
omartrese <omarlayachi13@gmail.com>
2023-03-29 19:01:58 +0200

    add-tasks

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
 addEventListener('DOMContentLoaded', () => {
 
-  document.getElementById('addTodo-button').addEventListener('click', () => {
+  getTasks();
 
-    alert('task saved succesfully!');
-
-  })
-
-
-
+  document.getElementById('addTodo-button').addEventListener('click', newTask);
   
+  let tasks = []
+  
+  function newTask(e) 
+  {
+    
+    let taskTitle = document.getElementById('todo-title').value;
+    let taskDescription = document.getElementById('todo-description').value;
+    
+    if(taskTitle === "")
+    {
+      alert('the task must contain a title');
+      return;
+    }
+
+    // console.log('The TITLE of the new task is: ' + taskTitle + '\n \n and the DESCRIPTION is: ' + taskDescription);
+
+
+    const task = {
+      title: taskTitle, 
+      description: taskDescription
+    };
+
+    console.log(task);
+
+    // if(localStorage.getItem('Tasks')[0] === null)
+    // {
+    //   tasks.push(task);
+    //   localStorage.setItem('Tasks', JSON.stringify(tasks));
+    // } else
+    // {
+      let tasks = JSON.parse(localStorage.getItem('Tasks'));
+      tasks.push(task);   
+      localStorage.setItem('Tasks', JSON.stringify(tasks));
+    // }
+
+    console.log(tasks);
+    e.preventDefault();
+    getTasks();
+    document.getElementById('todo-title').value = "";
+    document.getElementById('todo-description').value = "";
+    taskTitle = "";
+    taskDescription = "";
+  }
+  
+  function getTasks()
+  {
+    let newTasks = [];
+    newTasks = JSON.parse(localStorage.getItem('Tasks'));
+    let renderTasks = document.getElementById('todoTasks');
+
+    renderTasks.innerHTML = '';
+
+    for (let i = 0; i < newTasks.length; i++) 
+    {
+      let title = newTasks[i].title;
+      let description = newTasks[i].description;
+
+      if(description === "")
+      {
+        renderTasks.innerHTML += `<p>
+        ${title}</p>`
+      } else 
+      {
+        renderTasks.innerHTML += `<p>
+        ${title} -- ${description}</p>`
+      }
+
+    }
+  }
+
 })
